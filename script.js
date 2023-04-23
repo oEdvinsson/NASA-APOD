a05a4bbeb02dd1b2957580693b7e665184098289
Oscar Edvinsson <oscaredvinsson@gmail.com>
2023-04-23 20:24:04 +0200

    added functionality

--- script.js
+++ script.js
@@ -40,8 +40,13 @@ function createDOMNodes(page) {
         // Save Text
         const saveText = document.createElement('p');
         saveText.classList.add('clickable');
-        saveText.textContent = 'Add To Favorites';
-        saveText.setAttribute('onclick', `saveFavorite('${result.url}')`);
+        if (page === 'results') {
+            saveText.textContent = 'Add To Favorites';
+            saveText.setAttribute('onclick', `saveFavorite('${result.url}')`);
+        } else {
+            saveText.textContent = 'Remove Favorite';
+            saveText.setAttribute('onclick', `removeFavorite('${result.url}')`);
+        }
         // Card Text
         const cardText = document.createElement('p');
         cardText.textContent = result.explanation; 
@@ -78,6 +83,7 @@ async function getNasaPictures() {
     try {
         const response = await fetch(apiUrl);
         resultsArray = await response.json();
+        createDOMNodes('results');
         updateDOM('favorites');
     }  catch (error) {
         // Catch Error Here
